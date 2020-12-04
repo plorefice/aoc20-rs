1fd8bb12e8c61eb63056883ff69907a0986cc786
Pietro Lorefice <pietro@develer.com>
2020-12-04 18:34:33 +0100

    day4: optimize solution

--- src/day4.rs
+++ src/day4.rs
@@ -1,60 +1,85 @@
-use std::collections::HashMap;
+use std::str;
 
-const REQUIRED_KEYS: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+const REQUIRED_KEYS: [&[u8]; 7] = [b"byr", b"iyr", b"eyr", b"hgt", b"hcl", b"ecl", b"pid"];
 
-pub fn parse_input(input: &str) -> Vec<HashMap<&str, &str>> {
+pub fn parse_input(input: &str) -> Vec<Vec<(&[u8], &[u8])>> {
     input
         .split("\n\n")
         .map(|l| {
             l.split_ascii_whitespace()
                 .map(|w| {
                     let mut kv = w.split(':');
-                    (kv.next().unwrap(), kv.next().unwrap())
+                    (kv.next().unwrap().as_bytes(), kv.next().unwrap().as_bytes())
                 })
                 .collect()
         })
         .collect()
 }
 
-pub fn part_1(input: &[HashMap<&str, &str>]) -> usize {
+pub fn part_1(input: &[Vec<(&[u8], &[u8])>]) -> usize {
     input
         .iter()
-        .filter(|map| REQUIRED_KEYS.iter().all(|k| map.contains_key(k)))
+        .filter(|map| {
+            REQUIRED_KEYS
+                .iter()
+                .all(|key| map.iter().any(|(k, _)| key == k))
+        })
         .count()
 }
 
-pub fn part_2(input: &[HashMap<&str, &str>]) -> usize {
+pub fn part_2(input: &[Vec<(&[u8], &[u8])>]) -> usize {
     input
         .iter()
-        .filter(|map| REQUIRED_KEYS.iter().all(|k| map.contains_key(k)))
+        .filter(|map| {
+            REQUIRED_KEYS
+                .iter()
+                .all(|key| map.iter().any(|(k, _)| key == k))
+        })
         .filter(|map| {
             map.iter().all(|(k, v)| match *k {
-                "byr" => {
-                    let yr = v.parse::<u32>().unwrap();
+                b"byr" => {
+                    let yr = unsafe { str::from_utf8_unchecked(v) }
+                        .parse::<u32>()
+                        .unwrap();
                     yr >= 1920 && yr <= 2002
                 }
-                "iyr" => {
-                    let yr = v.parse::<u32>().unwrap();
+                b"iyr" => {
+                    let yr = unsafe { str::from_utf8_unchecked(v) }
+                        .parse::<u32>()
+                        .unwrap();
                     yr >= 2010 && yr <= 2020
                 }
-                "eyr" => {
-                    let yr = v.parse::<u32>().unwrap();
+                b"eyr" => {
+                    let yr = unsafe { str::from_utf8_unchecked(v) }
+                        .parse::<u32>()
+                        .unwrap();
                     yr >= 2020 && yr <= 2030
                 }
-                "hgt" if v.ends_with("in") => {
-                    let h = v.trim_end_matches("in").parse::<u32>().unwrap();
+                b"hgt" if v.ends_with(b"in") => {
+                    let h = unsafe { str::from_utf8_unchecked(&v[..v.len() - 2]) }
+                        .parse::<u32>()
+                        .unwrap();
                     h >= 59 && h <= 76
                 }
-                "hgt" if v.ends_with("cm") => {
-                    let h = v.trim_end_matches("cm").parse::<u32>().unwrap();
+                b"hgt" if v.ends_with(b"cm") => {
+                    let h = unsafe { str::from_utf8_unchecked(&v[..v.len() - 2]) }
+                        .parse::<u32>()
+                        .unwrap();
                     h >= 150 && h <= 193
                 }
-                "hcl" if v.len() == 7 && v.as_bytes()[0] == b'#' => {
-                    v.bytes().skip(1).all(|b| b.is_ascii_hexdigit())
-                }
-                "ecl" => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(v),
-                "pid" if v.len() == 9 => v.bytes().all(|b| b.is_ascii_digit()),
-                "cid" => true,
+                b"hcl" if v.len() == 7 && v[0] == b'#' => v[1..].iter().all(u8::is_ascii_hexdigit),
+                b"ecl" => [
+                    &b"amb"[..],
+                    &b"blu"[..],
+                    &b"brn"[..],
+                    &b"gry"[..],
+                    &b"grn"[..],
+                    &b"hzl"[..],
+                    &b"oth"[..],
+                ]
+                .contains(v),
+                b"pid" if v.len() == 9 => v.iter().all(u8::is_ascii_digit),
+                b"cid" => true,
                 _ => false,
             })
         })
