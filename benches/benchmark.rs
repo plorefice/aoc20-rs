f3120b74373021b40545437e7d53e46bded66e0d
Pietro Lorefice <pietro.lorefice@gmail.com>
2020-12-05 10:11:49 +0100

    Account for input parsing time in benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,24 +2,19 @@ use aoc20_rs::*;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn day_1(c: &mut Criterion) {
-    let input = day1::parse_input(include_str!("../inputs/day1.txt"));
-
     c.bench_function("day_1", |b| {
-        b.iter_with_setup(
-            || input.clone(),
-            |data| {
-                day1::part_1(black_box(&data));
-                day1::part_2(black_box(data));
-            },
-        )
+        b.iter(|| {
+            let input = day1::parse_input(include_str!("../inputs/day1.txt"));
+            day1::part_1(black_box(&input));
+            day1::part_2(black_box(input));
+        })
     });
 }
 
 pub fn day_2(c: &mut Criterion) {
-    let input = day2::parse_input(include_str!("../inputs/day2.txt"));
-
     c.bench_function("day_2", |b| {
         b.iter(|| {
+            let input = day2::parse_input(include_str!("../inputs/day2.txt"));
             day2::part_1(black_box(&input));
             day2::part_2(black_box(&input));
         })
@@ -27,10 +22,9 @@ pub fn day_2(c: &mut Criterion) {
 }
 
 pub fn day_3(c: &mut Criterion) {
-    let input = day3::parse_input(include_str!("../inputs/day3.txt"));
-
     c.bench_function("day_3", |b| {
         b.iter(|| {
+            let input = day3::parse_input(include_str!("../inputs/day3.txt"));
             day3::part_1(black_box(&input));
             day3::part_2(black_box(&input));
         })
@@ -38,10 +32,9 @@ pub fn day_3(c: &mut Criterion) {
 }
 
 pub fn day_4(c: &mut Criterion) {
-    let input = day4::parse_input(include_str!("../inputs/day4.txt"));
-
     c.bench_function("day_4", |b| {
         b.iter(|| {
+            let input = day4::parse_input(include_str!("../inputs/day4.txt"));
             day4::part_1(black_box(&input));
             day4::part_2(black_box(&input));
         })
